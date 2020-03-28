41e1f7018ff86d80da7e17831de62c1af3b1d7d7
Ygor AZambuja <ygorazambuja@gmail.com>
2020-03-28 13:25:42 -0400

    add "activeCases, activeCasesMildCondition, activeCasesSeriousCondition" params in /

--- main.py
+++ main.py
@@ -15,8 +15,11 @@ def main_route():
     confirmedCases = soup.find_all('div', class_='maincounter-number')[0].text.strip(' ').strip('\n')
     deathCases = soup.find_all('div', class_='maincounter-number')[1].text.strip(' ').strip('\n')
     recovered = soup.find_all('div', class_='maincounter-number')[2].text.strip(' ').strip('\n')
-    closedCases = soup.find('div', class_='number-table-main').text
+    closedCases = soup.find_all('div', class_='number-table-main')[1].text
     lastUpdate = soup.find_all('div', style='font-size:13px; color:#999; text-align:center')[0].text
+    activeCases = soup.find_all(class_='number-table-main')[0].text
+    activeCasesMildCondition = soup.find_all(class_='number-table')[0].text
+    activeCasesSeriousCondition = soup.find_all(class_='number-table')[1].text
 
     tbody = soup.find('tbody')
     rows = tbody.find_all('tr')
@@ -24,7 +27,9 @@ def main_route():
     countries = fill_country_object(rows)
 
     return {'confirmedCases': confirmedCases, 'deathCases': deathCases, "recovereds": recovered,
-            'closedCases': closedCases, 'countries': countries, 'lastUpdate': lastUpdate}
+            'closedCases': closedCases, 'countries': countries, 'lastUpdate': lastUpdate, 'activeCases': activeCases,
+            'activeCasesMildCondition': activeCasesMildCondition,
+            'activeCasesSeriousCondition': activeCasesSeriousCondition}
 
 
 @app.route('/country/<country>')
